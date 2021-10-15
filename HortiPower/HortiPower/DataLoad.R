7bd6f3a9ecb51c39b9ed0978cf5285ab8d933351
WitseMeeussen <g6fs2v3c5b74c4@gmail.com>
2021-10-15 11:45:26 +0200

    load binnendata

--- HortiPower/HortiPower/DataLoad.R
+++ HortiPower/HortiPower/DataLoad.R
@@ -1,26 +1,36 @@
 library(tidyverse)
+library(tibble)
 binnenDirs <- dir("../Data", pattern="Binnenmetingen(...)", full.names = TRUE)
 
-data <- read_delim("../Data/Buitenmetingen/Buiten_RVmeteo.txt",col_names=TRUE, col_types = NULL)
 
+# test
+#data2 <- read_delim("../Data/Binnenmetingen 1A/File1A_assimilatiebelichting.txt",col_names=TRUE, col_types = NULL, delim=";")
+#data3 <- read_delim("../Data/Binnenmetingen 1A/File1A_AVklimaat.txt",col_names=TRUE, col_types = NULL, delim=";")
+#data4 <- merge(data2,data3,by = intersect(c('date','time'),c('date','time')),incomparables = NULL)
+#data4
 # trying to use recursion to load the files but the nested loop works but is not verry readable
-readDir <- function(direction){
-  if(grepl(pattern=".txt", x=i)){
-    file = read_table(dir)
-  }else{
-    
-  }
-}
 # loops true the directions and tries to add the collumn. collumn not exact length it is screwed
-binnenDirs
+dat <- NULL
+fullBinnenData <- NULL
 for (i in binnenDirs){
   files <- list.files(i, full.names = TRUE)
+  groupNumber <- gsub('^(.*)\\s','',i)
   for(j in files){
     file <- read_delim(j,col_names=TRUE, col_types = NULL)
-    data %>% add_column(k = file[[2]])
+    if(is.null(dat)){
+      file[,4] <- groupNumber
+      dat <- file
+    } else{
+      noGroup <-file[,-4]
+      dat <- full_join(dat,noGroup,by = intersect(c('date','time'),c('date','time')))
+    }
+    
+  }
+  if(is.null(fullBinnenData)){
+    fullBinnenData <- dat
+  }else{
+    fullBinnenData <- bind_rows(fullBinnenData,dat)
   }
 }
 
-
-
-data
+# tables 
