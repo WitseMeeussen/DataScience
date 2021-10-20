6ac4f03c0c9dfd124dbaa473c8e93f57447ad9a8
WitseMeeussen <g6fs2v3c5b74c4@gmail.com>
2021-10-20 17:30:26 +0200

    horti recursive data load

--- HortiPower/HortiPower/DataLoad.R
+++ HortiPower/HortiPower/DataLoad.R
@@ -1,36 +1,44 @@
 library(tidyverse)
 library(tibble)
-binnenDirs <- dir("../Data", pattern="Binnenmetingen(...)", full.names = TRUE)
 
-
-# test
-#data2 <- read_delim("../Data/Binnenmetingen 1A/File1A_assimilatiebelichting.txt",col_names=TRUE, col_types = NULL, delim=";")
-#data3 <- read_delim("../Data/Binnenmetingen 1A/File1A_AVklimaat.txt",col_names=TRUE, col_types = NULL, delim=";")
-#data4 <- merge(data2,data3,by = intersect(c('date','time'),c('date','time')),incomparables = NULL)
-#data4
-# trying to use recursion to load the files but the nested loop works but is not verry readable
-# loops true the directions and tries to add the collumn. collumn not exact length it is screwed
-dat <- NULL
-fullBinnenData <- NULL
-for (i in binnenDirs){
-  files <- list.files(i, full.names = TRUE)
-  groupNumber <- gsub('^(.*)\\s','',i)
-  for(j in files){
-    file <- read_delim(j,col_names=TRUE, col_types = NULL)
-    if(is.null(dat)){
-      file[,4] <- groupNumber
-      dat <- file
-    } else{
-      noGroup <-file[,-4]
-      dat <- full_join(dat,noGroup,by = intersect(c('date','time'),c('date','time')))
-    }
-    
-  }
-  if(is.null(fullBinnenData)){
-    fullBinnenData <- dat
-  }else{
-    fullBinnenData <- bind_rows(fullBinnenData,dat)
-  }
+#  functions
+# returns a tibble that contains information of all the files joined (used to read Departments)
+recursive.ReadDepartment <- function(files){
+  file <-  read_delim(files[1],col_names=TRUE, col_types = NULL)
+  if(length(files) ==1){
+    file[,4] <- gsub('^(.*)\\s|/(.*)$','',files[1])
+    return (file)
+  } 
+  else return (full_join(file[,-4],recursive.ReadDepartment(files[-1]),by = intersect(c('date','time'),c('date','time'))))
+  
 }
 
-# tables 
+
+#returns a tibble containing every department info added together row wise
+recursive.ReadBinnenData <- function(dirs){
+  files <- list.files(dirs[1], full.names = TRUE)
+  if(length(dirs)==1) return (recursive.ReadDepartment(files)[['group']])
+  else return (bind_rows(recursive.ReadDepartment(files),recursive.ReadBinnenData(dirs[-1])))
+}
+
+recursive.ReadDir <- function(files){
+  file <-  read_delim(files[1],col_names=TRUE, col_types = NULL)
+  if(length(files) ==1)return (file[,-4]) 
+  else return (full_join(file[,-4],recursive.ReadDepartment(files[-1]),by = intersect(c('date','time'),c('date','time'))))
+  
+}
+
+#Reads the directories containing the measurements per department
+binnenDirs <- dir("../Data", pattern="Binnenmetingen(...)", full.names = TRUE)
+binnenData <- recursive.ReadBinnenData(binnenDirs)
+
+# excludes a non defining column group
+buitenData <- recursive.ReadDir(list.files("../Data/Buitenmetingen", full.names = TRUE))[,-9]
+
+#gelijktrekken op tijd
+plantDataLED <- read_table("../Data/Portento Belichting LED 2020.txt",col_names=c('dateS', 'timeS','sapstroom','dateD','timeD','diameter'), col_types = NULL, skip = 1)
+plantDataSonT <- read_table("../Data/Portento Belichting SonT 2020.txt",col_names=c('dateS', 'timeS','sapstroom','dateD','timeD','diameter'), col_types = NULL, skip = 1)
+#plantDataLED[1:3]
+#plantDataLED[4:6]
+
+#dataLed <- left_join(plantDataLED[1:3],plantDataLED[4:6],by = intersect(c('dateS'),c('dateD')))
