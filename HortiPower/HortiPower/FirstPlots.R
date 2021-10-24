db1c9d6e987579db21d3e01b22597464aff1df58
WitseMeeussen <g6fs2v3c5b74c4@gmail.com>
2021-10-24 16:47:06 +0200

    temp variation depending on wind conditions scatter plot

--- HortiPower/HortiPower/FirstPlots.R
+++ HortiPower/HortiPower/FirstPlots.R
@@ -12,13 +12,15 @@ windsnelheiddata <- buitenData %>%
   select(date,time,windsnelheid)%>%
   dplyr::filter(windsnelheid> 0)
 
+
+maxRaamstand <-  max(binnenData$luwezijderaamstand,na.rm = TRUE)
 tempvariation <-binnenData %>%
   dplyr::filter(kastemperatuurklimaat> 0)%>%
   group_by(date,time)%>%
-  mutate(sd = sd(kastemperatuurklimaat))%>%
-  mutate(wind = sum(windzijderaamstand))%>%
-  select(date,time,sd,windzijderaamstand,luwezijderaamstand,wind)%>%
+  mutate(TemperatuurDeviatie = sd(kastemperatuurklimaat))%>%
+  mutate(raaminvloet = sum(luwezijderaamstand,na.rm = TRUE)/maxRaamstand)%>%
+  select(date,time,TemperatuurDeviatie,windzijderaamstand,luwezijderaamstand,raaminvloet)%>%
   full_join(windsnelheiddata,by = intersect(c('date','time'),c('date','time')))
 
 ggplot(data=tempvariation) +
-  geom_point(mapping = aes(x = sd, y=windsnelheid*wind,alpha=0.1,))
+  geom_point(mapping = aes(x = TemperatuurDeviatie, y=windsnelheid*raaminvloet),alpha=0.1)
